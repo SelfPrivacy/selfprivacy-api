f228db5b299d562f27ce7ef976f42042bc6bb73b
inexcode <inex.code@selfprivacy.org>
2022-02-16 15:50:12 +0300

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selfprivacy_api",
-    version="1.1.1",
+    version="1.2.0",
     packages=find_packages(),
     scripts=[
         "selfprivacy_api/app.py",
