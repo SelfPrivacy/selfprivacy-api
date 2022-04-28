f2c73853bca0b05468c1039d828ff3202fa73e19
Inex Code <inex.code@selfprivacy.org>
2022-04-28 16:22:13 +0300

    Add migration to selfprivacy nix channel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selfprivacy_api",
-    version="1.2.2",
+    version="1.2.3",
     packages=find_packages(),
     scripts=[
         "selfprivacy_api/app.py",
