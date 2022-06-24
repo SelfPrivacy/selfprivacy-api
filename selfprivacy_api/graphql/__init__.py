766edc657a69e8170c76b7ed3646923ebce56f20
Inex Code <inex.code@selfprivacy.org>
2022-06-24 19:24:10 +0300

    resolve circular import

--- selfprivacy_api/graphql/__init__.py
+++ selfprivacy_api/graphql/__init__.py
@@ -2,9 +2,18 @@
 # pylint: disable=too-few-public-methods
 import typing
 import strawberry
+from selfprivacy_api.graphql.queries.api import ApiDevice, ApiRecoveryKeyStatus
 
 from selfprivacy_api.graphql.queries.system import System
-from selfprivacy_api.graphql.queries.api import Api
+
+from selfprivacy_api.resolvers.api import get_api_version, get_devices, get_recovery_key_status
+
+@strawberry.type
+class Api:
+    """API access status"""
+    version: str = strawberry.field(resolver=get_api_version)
+    devices: typing.List[ApiDevice] = strawberry.field(resolver=get_devices)
+    recovery_key: ApiRecoveryKeyStatus = strawberry.field(resolver=get_recovery_key_status)
 
 @strawberry.type
 class Query:
--- selfprivacy_api/graphql/queries/api.py
+++ selfprivacy_api/graphql/queries/api.py
@@ -4,7 +4,6 @@ import datetime
 import typing
 import strawberry
 
-from selfprivacy_api.resolvers.api import get_api_version, get_devices, get_recovery_key_status
 
 @strawberry.type
 class ApiDevice:
@@ -21,10 +20,3 @@ class ApiRecoveryKeyStatus:
     creation_date: typing.Optional[datetime.datetime]
     expiration_date: typing.Optional[datetime.datetime]
     uses_left: typing.Optional[int]
-
-@strawberry.type
-class Api:
-    """API access status"""
-    version: str = strawberry.field(resolver=get_api_version)
-    devices: typing.List[ApiDevice] = strawberry.field(resolver=get_devices)
-    recovery_key: ApiRecoveryKeyStatus = strawberry.field(resolver=get_recovery_key_status)
