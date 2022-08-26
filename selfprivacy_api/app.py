32278e9063149ba76375c4c52e514b7cf8f4abf6
inexcode <inex.code@selfprivacy.org>
2022-08-26 21:01:14 +0400

    Bind to 127.0.0.1 instead of 0.0.0.0

--- selfprivacy_api/app.py
+++ selfprivacy_api/app.py
@@ -53,4 +53,6 @@ async def startup():
 
 
 if __name__ == "__main__":
-    uvicorn.run("selfprivacy_api.app:app", host="0.0.0.0", port=5050, log_level="info")
+    uvicorn.run(
+        "selfprivacy_api.app:app", host="127.0.0.1", port=5050, log_level="info"
+    )
