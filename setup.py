24353ca56acddd4a75c37326b0339b9ee2584eb2
Inex Code <inex.code@selfprivacy.org>
2022-12-30 21:10:55 +0300

    chore: bump API version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selfprivacy_api",
-    version="2.0.0",
+    version="2.1.0",
     packages=find_packages(),
     scripts=[
         "selfprivacy_api/app.py",
