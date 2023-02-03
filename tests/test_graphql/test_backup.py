b6650b52c389d70f355bf0182224ea702562612f
Houkime <>
2023-02-03 18:49:24 +0000

    test(backup): init an in-memory backup class

--- tests/test_graphql/test_backup.py
+++ tests/test_graphql/test_backup.py
@@ -14,12 +14,22 @@ def test_service(tmpdir):
     return DummyService(tmpdir)
 
 
+@pytest.fixture()
+def memory_backup():
+    ProviderClass = providers.get_provider(BackupProvider.MEMORY)
+    assert ProviderClass is not None
+    memory_provider = ProviderClass(login="", key="")
+    assert memory_provider is not None
+    return memory_provider
+
+
 def test_select_backend():
     provider = providers.get_provider(BackupProvider.BACKBLAZE)
     assert provider is not None
     assert provider == Backblaze
 
 
-def test_backup(test_service):
+def test_backup_service(test_service, memory_backup):
     # temporarily incomplete
     assert test_service is not None
+    assert memory_backup is not None
