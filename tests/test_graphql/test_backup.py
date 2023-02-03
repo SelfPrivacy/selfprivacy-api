934afeb5318a7c88adb22ad03c38f556f1da9e7a
Houkime <>
2023-02-03 17:04:35 +0000

    test(backup): dummy service

--- tests/test_graphql/test_backup.py
+++ tests/test_graphql/test_backup.py
@@ -1,3 +1,7 @@
+import pytest
+
+from selfprivacy_api.services.test_service import DummyService
+
 import selfprivacy_api.backup.providers as providers
 from selfprivacy_api.backup.providers import AbstractBackupProvider
 
@@ -5,7 +9,17 @@ from selfprivacy_api.backup.providers.backblaze import Backblaze
 from selfprivacy_api.graphql.queries.providers import BackupProvider
 
 
+@pytest.fixture()
+def test_service(tmpdir):
+    return DummyService(tmpdir)
+
+
 def test_select_backend():
     provider = providers.get_provider(BackupProvider.BACKBLAZE)
     assert provider is not None
     assert provider == Backblaze
+
+
+def test_backup(test_service):
+    # temporarily incomplete
+    assert test_service is not None
