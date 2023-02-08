6fa72dbac1f2c44af75377b0b017773f19cfb3a4
Houkime <>
2023-02-08 14:04:55 +0000

    fix(backup): add memory backup class,forgot to add to git

---
+++ selfprivacy_api/backup/providers/memory.py
@@ -0,0 +1,6 @@
+from .provider import AbstractBackupProvider
+from selfprivacy_api.backup.restic_backuper import ResticBackuper
+
+
+class InMemoryBackup(AbstractBackupProvider):
+    backuper = ResticBackuper("", "", "memory")
