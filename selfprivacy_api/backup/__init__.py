54103973bce51cb4bef0509dcdc8a54eb2503319
Houkime <>
2023-02-08 14:05:25 +0000

    test(backup): try to back up!

--- selfprivacy_api/backup/__init__.py
+++ selfprivacy_api/backup/__init__.py
@@ -1,5 +1,10 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
 
 class AbstractBackuper(ABC):
     def __init__(self):
         pass
+
+    @abstractmethod
+    def start_backup(self, folder: str):
+        raise NotImplementedError
--- selfprivacy_api/backup/restic_backuper.py
+++ selfprivacy_api/backup/restic_backuper.py
@@ -18,44 +18,54 @@ class ResticBackuper(AbstractBackuper):
     def restic_repo(self, repository_name: str) -> str:
         # https://restic.readthedocs.io/en/latest/030_preparing_a_new_repo.html#other-services-via-rclone
         # https://forum.rclone.org/t/can-rclone-be-run-solely-with-command-line-options-no-config-no-env-vars/6314/5
-        return f"rclone::{self.type}:{self._repository_name}/sfbackup"
+        return f"rclone::{self.type}:{repository_name}/sfbackup"
 
     def rclone_args(self):
         return "rclone.args=serve restic --stdio" + self.backend_rclone_args()
 
-    def backend_rclone_args(self, account: str, key: str) -> str:
+    def backend_rclone_args(self) -> str:
         acc_arg = ""
         key_arg = ""
-        if account != "":
-            acc_arg = f"{self.login_flag} {account}"
-        if key != "":
-            key_arg = f"{self.key_flag} {key}"
+        if self.account != "":
+            acc_arg = f"{self.login_flag} {self.account}"
+        if self.key != "":
+            key_arg = f"{self.key_flag} {self.key}"
 
         return f"{acc_arg} {key_arg}"
 
-    def restic_command(self, account: str, key: str, *args):
-        return [
+    def restic_command(self, repo_name: str, *args):
+        command = [
             "restic",
             "-o",
             self.rclone_args(),
             "-r",
-            self.restic_repo(account, key),
-        ].extend(args)
+            self.restic_repo(repo_name),
+        ]
+        if args != []:
+            command.extend(args)
+        return command
 
-    def start_backup(self, folder: str):
+    def start_backup(self, folder: str, repo_name: str):
         """
         Start backup with restic
         """
         backup_command = self.restic_command(
-            self.account,
-            self.key,
+            repo_name,
             "backup",
             folder,
         )
-        with open("/var/backup.log", "w", encoding="utf-8") as log_file:
-            subprocess.Popen(
-                backup_command,
-                shell=False,
-                stdout=log_file,
-                stderr=subprocess.STDOUT,
-            )
+        subprocess.Popen(
+            backup_command,
+            shell=False,
+            stderr=subprocess.STDOUT,
+        )
+
+        # TODO: we might want to provide logging facilities
+        # that are reroutable for testing
+        # with open("/var/backup.log", "w", encoding="utf-8") as log_file:
+        #     subprocess.Popen(
+        #         backup_command,
+        #         shell=False,
+        #         stdout=log_file,
+        #         stderr=subprocess.STDOUT,
+        #     )
