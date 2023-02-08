7e243146db83a68d6237bef27b1bd4879d410877
Houkime <>
2023-02-08 14:57:34 +0000

    refactor(backup): add a backup function to Backups singleton class

--- selfprivacy_api/backup/__init__.py
+++ selfprivacy_api/backup/__init__.py
@@ -1,14 +1,40 @@
-from abc import ABC, abstractmethod
+from selfprivacy_api.utils.singleton_metaclass import SingletonMetaclass
+
+from selfprivacy_api.services.service import Service
+from selfprivacy_api.backup.providers.provider import AbstractBackupProvider
+from selfprivacy_api.backup.providers import get_provider
+from selfprivacy_api.graphql.queries.providers import BackupProvider
 
 
-class Backups:
+class Backups(metaclass=SingletonMetaclass):
     """A singleton controller for backups"""
 
+    provider: AbstractBackupProvider
 
-class AbstractBackuper(ABC):
     def __init__(self):
+        self.lookup_provider()
+
+    def lookup_provider(self):
+        redis_provider = Backups.load_provider_redis()
+        if redis_provider is not None:
+            self.provider = redis_provider
+
+        json_provider = Backups.load_provider_json()
+        if json_provider is not None:
+            self.provider = json_provider
+
+        provider_class = get_provider(BackupProvider.MEMORY)
+        self.provider = provider_class(login="", key="")
+
+    @staticmethod
+    def load_provider_redis() -> AbstractBackupProvider:
         pass
 
-    @abstractmethod
-    def start_backup(self, folder: str):
-        raise NotImplementedError
+    @staticmethod
+    def load_provider_json() -> AbstractBackupProvider:
+        pass
+
+    def back_up(self, service: Service):
+        folder = service.get_location()
+        repo_name = service.get_id()
+        self.provider.backuper.start_backup(folder, repo_name)
