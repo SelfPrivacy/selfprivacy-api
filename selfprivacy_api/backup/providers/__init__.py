5efb351159387c195bf7e6e2102c4d7e98157712
Houkime <>
2023-02-20 11:44:59 +0000

    feature(backups): register localfile backend

--- selfprivacy_api/backup/providers/__init__.py
+++ selfprivacy_api/backup/providers/__init__.py
@@ -3,10 +3,12 @@ from selfprivacy_api.backup.providers.provider import AbstractBackupProvider
 
 from selfprivacy_api.backup.providers.backblaze import Backblaze
 from selfprivacy_api.backup.providers.memory import InMemoryBackup
+from selfprivacy_api.backup.providers.local_file import LocalFileBackup
 
 PROVIDER_MAPPING = {
     BackupProvider.BACKBLAZE: Backblaze,
     BackupProvider.MEMORY: InMemoryBackup,
+    BackupProvider.FILE: LocalFileBackup,
 }
 
 
