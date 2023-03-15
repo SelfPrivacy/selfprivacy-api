09598033e74e14a587b6f742b93cc3acdec32995
Inex Code <inex.code@selfprivacy.org>
2023-03-15 14:09:47 +0300

    chore: Bump python to 3.10 and nixpkgs to 22.11

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixos-22.11> { } }:
 let
-  sp-python = pkgs.python39.withPackages (p: with p; [
+  sp-python = pkgs.python310.withPackages (p: with p; [
     setuptools
     portalocker
     pytz
@@ -19,45 +19,7 @@ let
     fastapi
     uvicorn
     redis
-    (buildPythonPackage rec {
-      pname = "strawberry-graphql";
-      version = "0.123.0";
-      format = "pyproject";
-      patches = [
-        ./strawberry-graphql.patch
-      ];
-      propagatedBuildInputs = [
-        typing-extensions
-        python-multipart
-        python-dateutil
-        # flask
-        pydantic
-        pygments
-        poetry
-        # flask-cors
-        (buildPythonPackage rec {
-          pname = "graphql-core";
-          version = "3.2.0";
-          format = "setuptools";
-          src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-huKgvgCL/eGe94OI3opyWh2UKpGQykMcJKYIN5c4A84=";
-          };
-          checkInputs = [
-            pytest-asyncio
-            pytest-benchmark
-            pytestCheckHook
-          ];
-          pythonImportsCheck = [
-            "graphql"
-          ];
-        })
-      ];
-      src = fetchPypi {
-        inherit pname version;
-        sha256 = "KsmZ5Xv8tUg6yBxieAEtvoKoRG60VS+iVGV0X6oCExo=";
-      };
-    })
+    strawberry-graphql
   ]);
 in
 pkgs.mkShell {
