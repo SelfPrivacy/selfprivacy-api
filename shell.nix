20402728798aa36f1bbcd0faf8060cd6268d3944
Houkime <>
2023-04-12 13:20:41 +0000

    fix(redis): Do not shut down redis on ctrl c
    see https://github.com/NixOS/nix/issues/2141

--- shell.nix
+++ shell.nix
@@ -35,7 +35,8 @@ pkgs.mkShell {
     # for example. printenv <Name> will not fetch the value of an attribute.
     export USE_REDIS_PORT=6379
     pkill redis-server
-    redis-server --bind 127.0.0.1 --port $USE_REDIS_PORT >/dev/null &
+    sleep 2
+    setsid redis-server --bind 127.0.0.1 --port $USE_REDIS_PORT >/dev/null 2>/dev/null &
     # maybe set more env-vars
   '';
 }
