3605a71c1dfa25a4abb1d861c9aad1b501d99d04
Houkime <>
2023-04-17 14:28:12 +0000

    refactor(services): remove special storage counting from ocserv

--- selfprivacy_api/services/ocserv/__init__.py
+++ selfprivacy_api/services/ocserv/__init__.py
@@ -4,7 +4,6 @@ import subprocess
 import typing
 from selfprivacy_api.jobs import Job, Jobs
 from selfprivacy_api.services.generic_service_mover import FolderMoveNames, move_service
-from selfprivacy_api.services.generic_size_counter import get_storage_usage
 from selfprivacy_api.services.generic_status_getter import get_service_status
 from selfprivacy_api.services.service import Service, ServiceDnsRecord, ServiceStatus
 from selfprivacy_api.utils import ReadUserData, WriteUserData
@@ -112,10 +111,6 @@ class Ocserv(Service):
                 ttl=3600,
             ),
         ]
-
-    @staticmethod
-    def get_storage_usage() -> int:
-        return 0
 
     @staticmethod
     def get_folders() -> typing.List[str]:
