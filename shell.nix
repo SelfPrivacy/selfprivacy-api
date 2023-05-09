13a2d8faca0704758a35230fc0412001ba5edc85
inexcode <inex.code@selfprivacy.org>
2023-05-09 11:54:47 +0300

    chore: Update dev nix shell

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixos-22.11> { } }:
 let
-  sp-python = pkgs.python39.withPackages (p: with p; [
+  sp-python = pkgs.python310.withPackages (p: with p; [
     setuptools
     portalocker
     pytz
@@ -19,45 +19,7 @@ let
     fastapi
     uvicorn
     redis
-    (buildPythonPackage rec {
-      pname = "strawberry-graphql";
-      version = "0.123.0";
-      format = "pyproject";
-      patches = [
-        ./strawberry-graphql.patch
-      ];
-      propagatedBuildInputs = [
-        typing-extensions
-        python-multipart
-        python-dateutil
-        # flask
-        pydantic
-        pygments
-        poetry
-        # flask-cors
-        (buildPythonPackage rec {
-          pname = "graphql-core";
-          version = "3.2.0";
-          format = "setuptools";
-          src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-huKgvgCL/eGe94OI3opyWh2UKpGQykMcJKYIN5c4A84=";
-          };
-          checkInputs = [
-            pytest-asyncio
-            pytest-benchmark
-            pytestCheckHook
-          ];
-          pythonImportsCheck = [
-            "graphql"
-          ];
-        })
-      ];
-      src = fetchPypi {
-        inherit pname version;
-        sha256 = "KsmZ5Xv8tUg6yBxieAEtvoKoRG60VS+iVGV0X6oCExo=";
-      };
-    })
+    strawberry-graphql
   ]);
 in
 pkgs.mkShell {
@@ -65,6 +27,7 @@ pkgs.mkShell {
     sp-python
     pkgs.black
     pkgs.redis
+    pkgs.restic
   ];
   shellHook = ''
     PYTHONPATH=${sp-python}/${sp-python.sitePackages}
@@ -72,8 +35,7 @@ pkgs.mkShell {
     # for example. printenv <Name> will not fetch the value of an attribute.
     export USE_REDIS_PORT=6379
     pkill redis-server
-    sleep 2
-    setsid redis-server --bind 127.0.0.1 --port $USE_REDIS_PORT >/dev/null 2>/dev/null & 
+    redis-server --bind 127.0.0.1 --port $USE_REDIS_PORT >/dev/null &
     # maybe set more env-vars
   '';
 }
