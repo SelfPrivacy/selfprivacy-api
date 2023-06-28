2dd9da9a962e84179809694f9e6459c90b7e8661
Houkime <>
2023-06-28 09:41:03 +0000

    fix(backups): register the correct tasks

--- selfprivacy_api/task_registry.py
+++ selfprivacy_api/task_registry.py
@@ -1,4 +1,4 @@
 from selfprivacy_api.utils.huey import huey
 from selfprivacy_api.jobs.test import test_job
-from selfprivacy_api.restic_controller.tasks import *
+from selfprivacy_api.backup.tasks import *
 from selfprivacy_api.services.generic_service_mover import move_service
