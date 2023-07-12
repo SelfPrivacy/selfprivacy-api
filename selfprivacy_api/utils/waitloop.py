e7a67005227a7c1a9d74b759460f7ca21e4512be
Houkime <>
2023-07-12 12:10:40 +0000

    test(backups): test async service start n stop simulation

--- selfprivacy_api/utils/waitloop.py
+++ selfprivacy_api/utils/waitloop.py
@@ -2,14 +2,19 @@ from time import sleep
 from typing import Callable
 from typing import Optional
 
-def wait_until_true(readiness_checker: Callable[[],bool],*,interval: float =0.1, timeout_sec: Optional[float] = None):
+
+def wait_until_true(
+    readiness_checker: Callable[[], bool],
+    *,
+    interval: float = 0.1,
+    timeout_sec: Optional[float] = None
+):
     elapsed = 0.0
     if timeout_sec is None:
         timeout_sec = 10e16
-    while not readiness_checker or elapsed > timeout_sec:
+
+    while (not readiness_checker()) and elapsed < timeout_sec:
         sleep(interval)
         elapsed += interval
     if elapsed > timeout_sec:
         raise TimeoutError()
-
-        
