2b21df9ad35855803ec227c56e88a8c962e97b39
Houkime <>
2024-01-03 19:30:27 +0000

    chore(version): bump version to 3.0, no Rest API

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selfprivacy_api",
-    version="2.4.3",
+    version="3.0.0",
     packages=find_packages(),
     scripts=[
         "selfprivacy_api/app.py",
