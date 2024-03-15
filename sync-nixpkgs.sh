64da8503dd4d8d51bc46f585904147da4edf8402
Houkime <>
2024-03-15 11:01:34 +0000

    chore(api): update nixpkgs version and add a script to do it

---
+++ sync-nixpkgs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# sync the version of nixpkgs used in the repo with one set in nixos-config 
+nix flake lock --override-input nixpkgs nixpkgs --inputs-from 'git+https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config.git?ref=flakes'
