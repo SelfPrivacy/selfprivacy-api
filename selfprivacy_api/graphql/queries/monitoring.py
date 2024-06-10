2e92557e8432fa54f1b5a533b0b47edd915046c4
def <dettlaff@riseup.net>
2024-06-10 05:27:37 +0300

    feat: add _send_request

--- selfprivacy_api/graphql/queries/monitoring.py
+++ selfprivacy_api/graphql/queries/monitoring.py
@@ -25,13 +25,15 @@ class MonitoringInfo:
 class Monitoring:
     """GraphQL queries to get prometheus monitoring information."""
 
-    @strawberry.field
-    def status(self) -> MonitoringInfo:
-        """Get status"""
-        endpoint = "/api/v1/status/runtimeinfo"
-
+    def _send_request(self, endpoint="/api/v1/query", query=None) -> MonitoringInfo:
         try:
-            response = requests.get(f"{PROMETHEUS_URL}{endpoint}")
+            if query:
+                response = requests.get(
+                    f"{PROMETHEUS_URL}{endpoint}", params={"query": query}
+                )
+            else:
+                response = requests.get(f"{PROMETHEUS_URL}{endpoint}")
+
             return MonitoringInfo(
                 http_response=response.status_code,
                 output=(
@@ -45,46 +47,24 @@ class Monitoring:
             )
 
     @strawberry.field
+    def status(self) -> MonitoringInfo:
+        """Get status"""
+        endpoint = "/api/v1/status/runtimeinfo"
+        return _send_request(endpoint=endpoint)
+
+    @strawberry.field
     def build(self) -> MonitoringInfo:
         """Get build information"""
         endpoint = "/api/v1/status/buildinfo"
+        return _send_request(endpoint=endpoint)
 
-        try:
-            response = requests.get(f"{PROMETHEUS_URL}{endpoint}")
-            return MonitoringInfo(
-                http_response=response.status_code,
-                output=(
-                    json.dumps(response.json()) if response.status_code == 200 else None
-                ),
-            )
-        except requests.RequestException as error:
-            return MonitoringInfo(
-                http_response=500,
-                error=str(error),
-            )
-
-    def cpu_usage(self):
+    @strawberry.field
+    def cpu_usage(self) -> MonitoringInfo:
         """Get CPU information"""
-        endpoint = "/api/v1/query"
         query = """100 - (avg by (instance) (irate(node_cpu_seconds_total{job="node",mode="idle"}[5m])) * 100)"""
         # TODO: не представляю рабочий ли этот запрос вообще
         # https://overflow.hostux.net/questions/34923788/prometheus-convert-cpu-user-seconds-to-cpu-usage
         # ему бы еще сделать кастомные минуты, чтоб разное время указывать, но так чтоб нельзя было инжектить
-
-        try:
-            response = requests.get(
-                f"{PROMETHEUS_URL}{endpoint}", params={"query": query}
-            )
-            return MonitoringInfo(
-                http_response=response.status_code,
-                output=(
-                    json.dumps(response.json()) if response.status_code == 200 else None
-                ),
-            )
-        except requests.RequestException as error:
-            return MonitoringInfo(
-                http_response=500,
-                error=str(error),
-            )
+        return _send_request(query=query)
 
     def disks_usage(): ...
