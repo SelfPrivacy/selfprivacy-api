60c5e129464d155a1e621280baa6dcdbfef50b7d
nhnn <nhnn@disroot.org>
2024-07-08 19:02:34 +0300

    fix: add type annotations to monitoring.py

--- selfprivacy_api/graphql/queries/monitoring.py
+++ selfprivacy_api/graphql/queries/monitoring.py
@@ -1,18 +1,18 @@
 import strawberry
 
-from selfprivacy_api.utils.prometheus import PrometheusQueries
+from selfprivacy_api.utils.prometheus import PrometheusQueries, PrometheusQueryResult
 
 
 @strawberry.type
 class Monitoring:
     @strawberry.field
-    def disk_usage():
+    def disk_usage() -> PrometheusQueryResult:
         return PrometheusQueries.disk_usage()
 
     @strawberry.field
-    def memory_usage():
+    def memory_usage() -> PrometheusQueryResult:
         return PrometheusQueries.memory_usage()
 
     @strawberry.field
-    def cpu_usage():
+    def cpu_usage() -> PrometheusQueryResult:
         return PrometheusQueries.cpu_usage()
