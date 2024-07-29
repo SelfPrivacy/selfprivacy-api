60ce3b773d052c33e7b43aa01827e7204d7847d4
Inex Code <inex.code@selfprivacy.org>
2024-07-29 18:50:23 +0300

    chore: Also show a query when prometheus fails

--- selfprivacy_api/utils/monitoring.py
+++ selfprivacy_api/utils/monitoring.py
@@ -70,7 +70,7 @@ class MonitoringQueries:
             )
             if response.status_code != 200:
                 return MonitoringQueryError(
-                    error=f"Prometheus returned unexpected HTTP status code. Error: {response.text}"
+                    error=f"Prometheus returned unexpected HTTP status code. Error: {response.text}. The query was {query}"
                 )
             json = response.json()
             if result_type and json["data"]["resultType"] != result_type:
@@ -96,7 +96,7 @@ class MonitoringQueries:
             )
             if response.status_code != 200:
                 return MonitoringQueryError(
-                    error=f"Prometheus returned unexpected HTTP status code. Error: {response.text}"
+                    error=f"Prometheus returned unexpected HTTP status code. Error: {response.text}. The query was {query}"
                 )
             json = response.json()
             if result_type and json["data"]["resultType"] != result_type:
