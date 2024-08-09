bbd909a54426d3ee254565df8766956aa8cd372f
dettlaff <dettlaff@riseup.net>
2024-08-09 23:10:21 +0400

    feat: timeout for monitoring

--- selfprivacy_api/utils/monitoring.py
+++ selfprivacy_api/utils/monitoring.py
@@ -67,6 +67,7 @@ class MonitoringQueries:
                     "end": end,
                     "step": step,
                 },
+                timeout=0.8,
             )
             if response.status_code != 200:
                 return MonitoringQueryError(
@@ -93,6 +94,7 @@ class MonitoringQueries:
                 params={
                     "query": query,
                 },
+                timeout=0.8,
             )
             if response.status_code != 200:
                 return MonitoringQueryError(
