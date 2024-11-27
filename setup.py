d91d8d2fd9fbc931f8f01b2cf7cc1d9dcc32f0d7
Inex Code <inex.code@selfprivacy.org>
2024-11-27 13:35:33 +0300

    chore: Bump version to 3.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selfprivacy_api",
-    version="3.3.0",
+    version="3.4.0",
     packages=find_packages(),
     scripts=[
         "selfprivacy_api/app.py",
