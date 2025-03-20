f7d7043a69fc52e00054afede85a107860cc9ef9
Inex Code <inex.code@selfprivacy.org>
2025-03-20 19:35:10 +0300

    refactor: Reorganize templates

--- selfprivacy_api/userpanel/routes/user.py
+++ selfprivacy_api/userpanel/routes/user.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
+from selfprivacy_api.models.user import UserDataUser
 from selfprivacy_api.userpanel.templates import templates
 from selfprivacy_api.userpanel.auth.session import Session, delete_session_token_cookie
 from selfprivacy_api.userpanel.routes.dependencies import get_current_user
+from selfprivacy_api.repositories.users.kanidm_user_repository import (
+    KanidmUserRepository,
+)
 from typing import Annotated
 
 import logging
@@ -14,11 +18,15 @@ router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
 async def get_profile(
-    request: Request, user: Annotated[Session, Depends(get_current_user)]
+    request: Request, session: Annotated[Session, Depends(get_current_user)]
 ):
-    logger.info(f"User {user}")
+    try:
+        user: UserDataUser = KanidmUserRepository.get_user_by_username(session.user_id)
+    except Exception as e:
+        logger.error(f"Error getting user by username: {e}")
+        raise HTTPException(status_code=500)
     return templates.TemplateResponse(
-        "profile.html", {"request": request, "username": user.user_id}
+        "profile.html", {"request": request, "user": user.model_dump()}
     )
 
 
