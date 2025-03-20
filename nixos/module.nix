c52e124741b50e01c6bdb939e88da36358942cda
Inex Code <inex.code@selfprivacy.org>
2025-03-20 14:50:41 +0300

    refactor: nixos module

--- nixos/module.nix
+++ nixos/module.nix
@@ -1,4 +1,10 @@
-selfprivacy-graphql-api: { config, lib, pkgs, ... }:
+selfprivacy-graphql-api:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.services.selfprivacy-api;
@@ -96,160 +102,205 @@ in
       };
     };
 
-    systemd.services.selfprivacy-api = {
-      description = "API Server used to control system from the mobile application";
-      environment = config.nix.envVars // {
-        HOME = "/root";
-        PYTHONUNBUFFERED = "1";
-      } // config.networking.proxy.envVars;
-      path = [
-        "/var/"
-        "/var/dkim/"
-        pkgs.coreutils
-        pkgs.gnutar
-        pkgs.xz.bin
-        pkgs.gzip
-        pkgs.gitMinimal
-        config.nix.package.out
-        pkgs.restic
-        pkgs.rclone
-        pkgs.mkpasswd
-        pkgs.util-linux
-        pkgs.e2fsprogs
-        pkgs.iproute2
-        pkgs.postgresql_16.out
-        sp-fetch-remote-module
-        pkgs.kanidm
-      ];
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        # Do not forget to edit Postgres identMap if you change the user!
-        User = "root";
-        ExecStart = "${selfprivacy-graphql-api}/bin/app.py";
-        Restart = "always";
-        RestartSec = "5";
-        Slice = "selfprivacy_api.slice";
+    systemd = {
+      services = {
+        kanidm.serviceConfig.ExecStartPre = lib.mkAfter [
+          ("-+" + kanidmExecStartPreScriptRoot)
+          ("-" + kanidmExecStartPreScript)
+        ];
+        selfprivacy-api = {
+          description = "API Server used to control system from the mobile application";
+          environment =
+            config.nix.envVars
+            // {
+              HOME = "/root";
+              PYTHONUNBUFFERED = "1";
+            }
+            // config.networking.proxy.envVars;
+          path = [
+            "/var/"
+            "/var/dkim/"
+            pkgs.coreutils
+            pkgs.gnutar
+            pkgs.xz.bin
+            pkgs.gzip
+            pkgs.gitMinimal
+            config.nix.package.out
+            pkgs.restic
+            pkgs.rclone
+            pkgs.mkpasswd
+            pkgs.util-linux
+            pkgs.e2fsprogs
+            pkgs.iproute2
+            pkgs.postgresql_16.out
+            sp-fetch-remote-module
+            pkgs.kanidm
+          ];
+          after = [ "network-online.target" ];
+          wants = [ "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            # Do not forget to edit Postgres identMap if you change the user!
+            User = "root";
+            ExecStart = "${selfprivacy-graphql-api}/bin/app.py";
+            Restart = "always";
+            RestartSec = "5";
+            Slice = "selfprivacy_api.slice";
+          };
+        };
+        selfprivacy-api-worker = {
+          description = "Task worker for SelfPrivacy API";
+          environment =
+            config.nix.envVars
+            // {
+              HOME = "/root";
+              PYTHONUNBUFFERED = "1";
+              PYTHONPATH = pkgs.python312Packages.makePythonPath [ selfprivacy-graphql-api ];
+            }
+            // config.networking.proxy.envVars;
+          path = [
+            "/var/"
+            "/var/dkim/"
+            pkgs.coreutils
+            pkgs.gnutar
+            pkgs.xz.bin
+            pkgs.gzip
+            pkgs.gitMinimal
+            config.nix.package.out
+            pkgs.restic
+            pkgs.rclone
+            pkgs.mkpasswd
+            pkgs.util-linux
+            pkgs.e2fsprogs
+            pkgs.iproute2
+            pkgs.postgresql_16.out
+            sp-fetch-remote-module
+            pkgs.kanidm
+          ];
+          after = [ "network-online.target" ];
+          wants = [ "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            # Do not forget to edit Postgres identMap if you change the user!
+            User = "root";
+            ExecStart = "${pkgs.python312Packages.huey}/bin/huey_consumer.py selfprivacy_api.task_registry.huey";
+            Restart = "always";
+            RestartSec = "5";
+            Slice = "selfprivacy_api.slice";
+          };
+        };
+        sp-nixos-rebuild = {
+          description = "nixos-rebuild switch";
+          environment =
+            config.nix.envVars
+            // {
+              HOME = "/root";
+            }
+            // config.networking.proxy.envVars;
+          # TODO figure out how to get dependencies list reliably
+          path = [
+            pkgs.coreutils
+            pkgs.gnutar
+            pkgs.xz.bin
+            pkgs.gzip
+            pkgs.gitMinimal
+            config.nix.package.out
+          ];
+          # TODO set proper timeout for reboot instead of service restart
+          serviceConfig = {
+            User = "root";
+            WorkingDirectory = "/etc/nixos";
+            # sync top-level flake with sp-modules sub-flake
+            # (https://github.com/NixOS/nix/issues/9339)
+            ExecStartPre = ''
+              ${nix} flake lock --override-input sp-modules path:./sp-modules
+            '';
+            ExecStart = ''
+              ${nixos-rebuild} switch --flake .#${config-id}
+            '';
+            KillMode = "mixed";
+            SendSIGKILL = "no";
+          };
+          restartIfChanged = false;
+          unitConfig.X-StopOnRemoval = false;
+        };
+        sp-nixos-upgrade = {
+          # protection against simultaneous runs
+          after = [ "sp-nixos-rebuild.service" ];
+          description = "Upgrade NixOS and SP modules to latest versions";
+          environment =
+            config.nix.envVars
+            // {
+              HOME = "/root";
+            }
+            // config.networking.proxy.envVars;
+          # TODO figure out how to get dependencies list reliably
+          path = [
+            pkgs.coreutils
+            pkgs.gnutar
+            pkgs.xz.bin
+            pkgs.gzip
+            pkgs.gitMinimal
+            config.nix.package.out
+          ];
+          serviceConfig = {
+            User = "root";
+            WorkingDirectory = "/etc/nixos";
+            # TODO get URL from systemd template parameter?
+            ExecStartPre = ''
+              ${nix} flake update \
+              --override-input selfprivacy-nixos-config git+https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config.git?ref=flakes
+            '';
+            ExecStart = ''
+              ${nixos-rebuild} switch --flake .#${config-id}
+            '';
+            KillMode = "mixed";
+            SendSIGKILL = "no";
+          };
+          restartIfChanged = false;
+          unitConfig.X-StopOnRemoval = false;
+        };
+        sp-nixos-rollback = {
+          # protection against simultaneous runs
+          after = [
+            "sp-nixos-rebuild.service"
+            "sp-nixos-upgrade.service"
+          ];
+          description = "Rollback NixOS using nixos-rebuild";
+          environment =
+            config.nix.envVars
+            // {
+              HOME = "/root";
+            }
+            // config.networking.proxy.envVars;
+          # TODO figure out how to get dependencies list reliably
+          path = [
+            pkgs.coreutils
+            pkgs.gnutar
+            pkgs.xz.bin
+            pkgs.gzip
+            pkgs.gitMinimal
+            config.nix.package.out
+          ];
+          serviceConfig = {
+            User = "root";
+            WorkingDirectory = "/etc/nixos";
+            ExecStart = ''
+              ${nixos-rebuild} switch --rollback --flake .#${config-id}
+            '';
+            KillMode = "mixed";
+            SendSIGKILL = "no";
+          };
+          restartIfChanged = false;
+          unitConfig.X-StopOnRemoval = false;
+        };
       };
-    };
-    systemd.services.selfprivacy-api-worker = {
-      description = "Task worker for SelfPrivacy API";
-      environment = config.nix.envVars // {
-        HOME = "/root";
-        PYTHONUNBUFFERED = "1";
-        PYTHONPATH =
-          pkgs.python312Packages.makePythonPath [ selfprivacy-graphql-api ];
-      } // config.networking.proxy.envVars;
-      path = [
-        "/var/"
-        "/var/dkim/"
-        pkgs.coreutils
-        pkgs.gnutar
-        pkgs.xz.bin
-        pkgs.gzip
-        pkgs.gitMinimal
-        config.nix.package.out
-        pkgs.restic
-        pkgs.rclone
-        pkgs.mkpasswd
-        pkgs.util-linux
-        pkgs.e2fsprogs
-        pkgs.iproute2
-        pkgs.postgresql_16.out
-        sp-fetch-remote-module
-        pkgs.kanidm
-      ];
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        # Do not forget to edit Postgres identMap if you change the user!
-        User = "root";
-        ExecStart = "${pkgs.python312Packages.huey}/bin/huey_consumer.py selfprivacy_api.task_registry.huey";
-        Restart = "always";
-        RestartSec = "5";
-        Slice = "selfprivacy_api.slice";
+      slices = {
+        "selfprivacy_api" = {
+          name = "selfprivacy_api.slice";
+          description = "Slice for SelfPrivacy API services";
+        };
       };
-    };
-    systemd.slices."selfprivacy_api" = {
-      name = "selfprivacy_api.slice";
-      description = "Slice for SelfPrivacy API services";
-    };
-    # One shot systemd service to rebuild NixOS using nixos-rebuild
-    systemd.services.sp-nixos-rebuild = {
-      description = "nixos-rebuild switch";
-      environment = config.nix.envVars // {
-        HOME = "/root";
-      } // config.networking.proxy.envVars;
-      # TODO figure out how to get dependencies list reliably
-      path = [ pkgs.coreutils pkgs.gnutar pkgs.xz.bin pkgs.gzip pkgs.gitMinimal config.nix.package.out ];
-      # TODO set proper timeout for reboot instead of service restart
-      serviceConfig = {
-        User = "root";
-        WorkingDirectory = "/etc/nixos";
-        # sync top-level flake with sp-modules sub-flake
-        # (https://github.com/NixOS/nix/issues/9339)
-        ExecStartPre = ''
-          ${nix} flake lock --override-input sp-modules path:./sp-modules
-        '';
-        ExecStart = ''
-          ${nixos-rebuild} switch --flake .#${config-id}
-        '';
-        KillMode = "mixed";
-        SendSIGKILL = "no";
-      };
-      restartIfChanged = false;
-      unitConfig.X-StopOnRemoval = false;
-    };
-    # One shot systemd service to upgrade NixOS using nixos-rebuild
-    systemd.services.sp-nixos-upgrade = {
-      # protection against simultaneous runs
-      after = [ "sp-nixos-rebuild.service" ];
-      description = "Upgrade NixOS and SP modules to latest versions";
-      environment = config.nix.envVars // {
-        HOME = "/root";
-      } // config.networking.proxy.envVars;
-      # TODO figure out how to get dependencies list reliably
-      path = [ pkgs.coreutils pkgs.gnutar pkgs.xz.bin pkgs.gzip pkgs.gitMinimal config.nix.package.out ];
-      serviceConfig = {
-        User = "root";
-        WorkingDirectory = "/etc/nixos";
-        # TODO get URL from systemd template parameter?
-        ExecStartPre = ''
-          ${nix} flake update \
-          --override-input selfprivacy-nixos-config git+https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config.git?ref=flakes
-        '';
-        ExecStart = ''
-          ${nixos-rebuild} switch --flake .#${config-id}
-        '';
-        KillMode = "mixed";
-        SendSIGKILL = "no";
-      };
-      restartIfChanged = false;
-      unitConfig.X-StopOnRemoval = false;
-    };
-    # One shot systemd service to rollback NixOS using nixos-rebuild
-    systemd.services.sp-nixos-rollback = {
-      # protection against simultaneous runs
-      after = [ "sp-nixos-rebuild.service" "sp-nixos-upgrade.service" ];
-      description = "Rollback NixOS using nixos-rebuild";
-      environment = config.nix.envVars // {
-        HOME = "/root";
-      } // config.networking.proxy.envVars;
-      # TODO figure out how to get dependencies list reliably
-      path = [ pkgs.coreutils pkgs.gnutar pkgs.xz.bin pkgs.gzip pkgs.gitMinimal config.nix.package.out ];
-      serviceConfig = {
-        User = "root";
-        WorkingDirectory = "/etc/nixos";
-        ExecStart = ''
-          ${nixos-rebuild} switch --rollback --flake .#${config-id}
-        '';
-        KillMode = "mixed";
-        SendSIGKILL = "no";
-      };
-      restartIfChanged = false;
-      unitConfig.X-StopOnRemoval = false;
     };
 
     services = {
