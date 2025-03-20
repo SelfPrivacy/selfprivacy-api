4a66e7ee0bb316c60730d1b482b7bcf935e73b2d
dettlaff <dettlaff@riseup.net>
2025-03-20 21:50:07 +0400

    feat: add utils for self service portal

--- selfprivacy_api/actions/email_passwords.py
+++ selfprivacy_api/actions/email_passwords.py
@@ -8,22 +8,9 @@ from selfprivacy_api.repositories.email_password import ACTIVE_EMAIL_PASSWORD_PR
 from passlib.hash import argon2
 
 
-def _generate_password_hash(password: str) -> str:
-    return argon2.hash(password)
-
-
 def get_email_credentials_metadata(username: str) -> list[EmailPasswordData]:
     return ACTIVE_EMAIL_PASSWORD_PROVIDER.get_all_email_passwords_metadata(
         username=username,
-    )
-
-
-def get_email_credentials_metadata_with_passwords_hashes(
-    username: str,
-) -> list[EmailPasswordData]:
-    return ACTIVE_EMAIL_PASSWORD_PROVIDER.get_all_email_passwords_metadata(
-        username=username,
-        with_passwords_hashes=True,
     )
 
 
@@ -36,7 +23,7 @@ def add_email_password(
 ) -> None:
     add_email_password_hash(
         username=username,
-        password_hash=_generate_password_hash(password=password),
+        password_hash=argon2.hash(password),
         display_name=display_name,
         with_created_at=with_created_at,
         with_zero_uuid=with_zero_uuid,
@@ -84,19 +71,3 @@ def update_legacy_email_password_hash(
 
 def delete_all_email_passwords_hashes(username: str):
     ACTIVE_EMAIL_PASSWORD_PROVIDER.delete_all_email_passwords_hashes(username)
-
-
-def validate_email_password(username: str, password: str) -> bool:
-    email_passwords_data = (
-        ACTIVE_EMAIL_PASSWORD_PROVIDER.get_all_email_passwords_metadata(
-            username=username,
-            with_passwords_hashes=True,
-        )
-    )
-    if not email_passwords_data:
-        return False
-
-    for i in email_passwords_data:
-        if argon2.verify(password, i.hash):
-            return True
-    return False
