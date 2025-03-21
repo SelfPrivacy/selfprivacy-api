ea64c4b5139a76878a9c8953c362464db2469a95
dettlaff <dettlaff@riseup.net>
2025-03-21 14:47:34 +0400

    fix: after bad merge

--- selfprivacy_api/utils/self_service_portal_utils.py
+++ selfprivacy_api/utils/self_service_portal_utils.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import secrets
-import base64
-import unicodedata
-
-from passlib.hash import argon2, sha512_crypt
-
-from selfprivacy_api.repositories.email_password import ACTIVE_EMAIL_PASSWORD_PROVIDER
-from selfprivacy_api.models.email_password_metadata import EmailPasswordData
-
-
-def generate_urlsave_password() -> str:
-    random_bytes = secrets.token_bytes(32)
-    return base64.urlsafe_b64encode(random_bytes).decode("utf-8")
-
-
-def generate_password_hash(password: str) -> str:
-    return argon2.hash(password)
-
-
-def verify_password(password: str, password_hash: str) -> bool:
-    password = unicodedata.normalize("NFKC", password)
-
-    if "$argon2" in password_hash:
-        return argon2.verify(password, password_hash)
-    else:
-        return sha512_crypt.verify(password, password_hash)
-=======
 from datetime import datetime, timezone
 from typing import Optional
 import logging
@@ -39,7 +11,6 @@ from selfprivacy_api.utils.argon2 import (
 from selfprivacy_api.actions.email_passwords import add_email_password
 
 logger = logging.getLogger(__name__)
->>>>>>> d5eaf399d26f7350239b524ac291a6b5711b6b72
 
 
 def get_email_credentials_metadata_with_passwords_hashes(
@@ -51,13 +22,10 @@ def get_email_credentials_metadata_with_passwords_hashes(
     )
 
 
-<<<<<<< HEAD
-=======
 def is_expired(expires_at: Optional[datetime]) -> bool:
     return expires_at is not None and expires_at < datetime.now(timezone.utc)
 
 
->>>>>>> d5eaf399d26f7350239b524ac291a6b5711b6b72
 def validate_email_password(username: str, password: str) -> bool:
     email_passwords_data = (
         ACTIVE_EMAIL_PASSWORD_PROVIDER.get_all_email_passwords_metadata(
@@ -69,11 +37,6 @@ def validate_email_password(username: str, password: str) -> bool:
         return False
 
     for i in email_passwords_data:
-<<<<<<< HEAD
-        if verify_password(password=password, password_hash=str(i.hash)):
-            return True
-    return False
-=======
         if i.password is None:
             continue
         if is_expired(i.expires_at):
@@ -101,4 +64,3 @@ def generate_new_email_password(
         expires_at=expires_at,
     )
     return password
->>>>>>> d5eaf399d26f7350239b524ac291a6b5711b6b72
