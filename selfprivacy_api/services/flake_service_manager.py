0246a72bc32935d70e6a1392017d3fb5a087f7b0
nhnn <nhnn@nhnn.dev>
2025-07-23 14:15:02 +0300

    feat(WIP): merge sp-modules flake and main flake in migration

--- selfprivacy_api/services/flake_service_manager.py
+++ selfprivacy_api/services/flake_service_manager.py
@@ -1,55 +1,64 @@
 import re
 from typing import Tuple, Optional
+from selfprivacy_api.utils.nix import evaluate_nix_file, to_nix_expr
 
-FLAKE_CONFIG_PATH = "/etc/nixos/sp-modules/flake.nix"
+FLAKE_CONFIG_PATH = "/etc/nixos/flake.nix"
+SP_MODULE_INPUT_PREFIX = "sp-module-"
 
 
 class FlakeServiceManager:
     def __enter__(self) -> "FlakeServiceManager":
         self.services = {}
 
-        with open(FLAKE_CONFIG_PATH, "r") as file:
-            for line in file:
-                service_name, url = self._extract_services(input_string=line)
-                if service_name and url:
-                    self.services[service_name] = url
+        inputs = evaluate_nix_file(FLAKE_CONFIG_PATH, "f: f.inputs")
+
+        for key, value in inputs.items():
+            if key.startswith(SP_MODULE_INPUT_PREFIX):
+                service_name = key.removeprefix(SP_MODULE_INPUT_PREFIX)
+                self.services[service_name] = value["url"]
 
         return self
 
-    def _extract_services(
-        self, input_string: str
-    ) -> Tuple[Optional[str], Optional[str]]:
-        pattern = r"inputs\.([\w-]+)\.url\s*=\s*([\S]+);"
-        match = re.search(pattern, input_string)
-
-        if match:
-            variable_name = match.group(1)
-            url = match.group(2)
-            if url.startswith('"') and url.endswith('"'):
-                return variable_name, url[1:-1]
-            return variable_name, url
-        else:
-            return None, None
-
     def __exit__(self, exc_type, exc_value, traceback) -> None:
+        inputs = {}
+        inputs["selfprivacy-nixos-config"] = {
+            # TODO: make it configurable
+            "url": "git+https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config.git?ref=flakes",
+            "inputs": {"selfprivacy-api": {"follows": "sp-api"}},
+        }
+        inputs["sp-api"] = {
+            "url": "git+https://git.selfprivacy.org/SelfPrivacy/selfprivacy-rest-api?ref=nhnn/sp-modules-flake-merge",
+            "inputs": {"nixpkgs": {"follows": "selfprivacy-nixos-config/nixpkgs"}},
+        }
+        for service_name, url in self.services.items():
+            inputs[f"{SP_MODULE_INPUT_PREFIX}{service_name}"] = {"url": url}
+
+        inputs_expr = to_nix_expr(inputs)
+
         with open(FLAKE_CONFIG_PATH, "w") as file:
             file.write(
                 """
 {
-  description = "SelfPrivacy NixOS PoC modules/extensions/bundles/packages/etc";\n
+  description = "SelfPrivacy NixOS configuration local flake";
 """
             )
 
-            for key, value in self.services.items():
-                file.write(
-                    f"""
-  inputs.{key}.url = "{value}";
-"""
-                )
+            file.write(f"\n  inputs = {inputs_expr};\n")
 
             file.write(
                 """
-  outputs = _: { };
+  outputs = inputs@{ self, selfprivacy-nixos-config, ... }: let
+    lib = selfprivacy-nixos-config.inputs.nixpkgs.lib;
+  in {
+    nixosConfigurations =
+      selfprivacy-nixos-config.outputs.nixosConfigurations-fun {
+        hardware-configuration = ./hardware-configuration.nix;
+        deployment = ./deployment.nix;
+        userdata = builtins.fromJSON (builtins.readFile ./userdata.json);
+        top-level-flake = self;
+        sp-modules = lib.mapAttrs' (service: value: { name = lib.removePrefix "sp-module-" service; inherit value; }) (lib.filterAttrs (k: _: lib.hasPrefix "sp-module-" k) inputs);
+      };
+  };
 }
 """
             )
