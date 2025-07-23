6981433ee537188e8fa907e6f6ffa925d23f1602
nhnn <nhnn@nhnn.dev>
2025-07-23 15:10:53 +0300

    fix: format flake.nix on save, not just inputs expression

--- selfprivacy_api/services/flake_service_manager.py
+++ selfprivacy_api/services/flake_service_manager.py
@@ -1,6 +1,6 @@
 import re
 from typing import Tuple, Optional
-from selfprivacy_api.utils.nix import evaluate_nix_file, to_nix_expr
+from selfprivacy_api.utils.nix import evaluate_nix_file, to_nix_expr, format_nix_expr
 
 FLAKE_CONFIG_PATH = "/etc/nixos/flake.nix"
 SP_MODULE_INPUT_PREFIX = "sp-module-"
@@ -35,18 +35,11 @@ class FlakeServiceManager:
 
         inputs_expr = to_nix_expr(inputs)
 
-        with open(FLAKE_CONFIG_PATH, "w") as file:
-            file.write(
-                """
-{
+        content += """{
   description = "SelfPrivacy NixOS configuration local flake";
 """
-            )
-
-            file.write(f"\n  inputs = {inputs_expr};\n")
-
-            file.write(
-                """
+        content += f"\n  inputs = {inputs_expr};\n"
+        content += """
   outputs = inputs@{ self, selfprivacy-nixos-config, ... }: let
     lib = selfprivacy-nixos-config.inputs.nixpkgs.lib;
   in {
@@ -56,9 +49,15 @@ class FlakeServiceManager:
         deployment = ./deployment.nix;
         userdata = builtins.fromJSON (builtins.readFile ./userdata.json);
         top-level-flake = self;
-        sp-modules = lib.mapAttrs' (service: value: { name = lib.removePrefix "sp-module-" service; inherit value; }) (lib.filterAttrs (k: _: lib.hasPrefix "sp-module-" k) inputs);
+        sp-modules = lib.mapAttrs'
+          (service: value: { name = lib.removePrefix "sp-module-" service; inherit value; })
+          (lib.filterAttrs (k: _: lib.hasPrefix "sp-module-" k) inputs);
       };
   };
 }
 """
-            )
+
+        content = format_nix_expr(content)
+
+        with open(FLAKE_CONFIG_PATH, "w") as file:
+            file.write(content)
