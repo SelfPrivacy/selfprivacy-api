6f442375eacbffb3bec0a4b461f4a850ab10b5ce
nhnn <nhnn@nhnn.dev>
2025-07-23 14:30:06 +0300

    fix: add old migration imports back

--- selfprivacy_api/migrations/__init__.py
+++ selfprivacy_api/migrations/__init__.py
@@ -17,6 +17,9 @@ from selfprivacy_api.migrations.write_token_to_redis import WriteTokenToRedis
 from selfprivacy_api.migrations.check_for_system_rebuild_jobs import (
     CheckForSystemRebuildJobs,
 )
+
+from selfprivacy_api.migrations.add_roundcube import AddRoundcube
+from selfprivacy_api.migrations.add_monitoring import AddMonitoring
 from selfprivacy_api.migrations.migrate_users_from_json import MigrateUsersFromJson
 from selfprivacy_api.migrations.add_postgres_location import AddPostgresLocation
 from selfprivacy_api.migrations.replace_blockdevices_to_uuid import (
