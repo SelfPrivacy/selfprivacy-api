865da38288c9ffcf6cda4e0b4c1b787a81cf62cc
dettlaff <dettlaff@riseup.net>
2025-10-14 00:53:31 +0400

    docs: upd ru translations

--- gettext_extract.sh
+++ gettext_extract.sh
@@ -28,3 +28,4 @@ xgettext \
   selfprivacy_api/jobs/upgrade_system.py \
   selfprivacy_api/jobs/migrate_to_binds.py \
   selfprivacy_api/models/services.py \
+  selfprivacy_api/jobs/nix_collect_garbage.py \
