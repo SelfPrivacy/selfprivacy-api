a6f662ef82803172844718f882fd7a180eef7ac6
dettlaff <dettlaff@riseup.net>
2025-10-25 13:19:35 +0400

    feat: change switching kanidm_credential_type to direct kanidm api, not json userdata

--- selfprivacy_api/actions/kanidm_credential_type.py
+++ selfprivacy_api/actions/kanidm_credential_type.py
@@ -1,61 +1,46 @@
 import logging
-
-from selfprivacy_api.utils import ReadUserData, WriteUserData
+import subprocess
+import re
 
 from selfprivacy_api.models.kanidm_credential_type import KanidmCredentialType
 
 
 logger = logging.getLogger(__name__)
 
-
-DEFAULT_KANIDM_MINIMUM_CREDENTIAL_TYPE = KanidmCredentialType.mfa
-
-
-class InvalidKanidmCredentialType(Exception):
-    """Invalid Kanidm Credential Type"""
-
-    @staticmethod
-    def get_error_message() -> str:
-        return """
-        Invalid value 'minimum_credential_type'
-        for enum 'KanidmCredentialLvl' in /etc/nixos/userdata.json.
-        Support only 'any', 'mfa', 'passkey'.
-        See https://kanidm.github.io/kanidm/stable/accounts/account_policy.html
-        """
+# TODO: admin token
 
 
 def get_kanidm_minimum_credential_type() -> KanidmCredentialType:
-    with ReadUserData() as data:
-        if "kanidm" in data:
-            if "minimum_credential_type" in data["kanidm"]:
-                raw = data["kanidm"]["minimum_credential_type"]
-                try:
-                    return KanidmCredentialType(str(raw))
-                except ValueError:
-                    raise InvalidKanidmCredentialType(f"Unsupported level: {raw}")
+    result = subprocess.check_output(
+        ["kanidm", "group", "get", "idm_all_persons"],
+        stderr=subprocess.STDOUT,
+    ).decode("utf-8", "replace")
 
-        logger.warning(
-            """
-            Missing 'kanidm/minimum_credential_type' key in /etc/nixos/userdata.json.
-            Setting the default value.
-            """
-        )
-        set_kanidm_minimum_credential_type(
-            minimum_credential_type=DEFAULT_KANIDM_MINIMUM_CREDENTIAL_TYPE
-        )
-        return DEFAULT_KANIDM_MINIMUM_CREDENTIAL_TYPE
+    match = re.search(r"(?mi)^\s*credential_type_minimum\s*:\s*(\S+)", result)
+    if match:
+        if {match.group(1)}:
+            return KanidmCredentialType({match.group(1)})
+
+    raise RuntimeError()  # TODO: replace this error after https://git.selfprivacy.org/SelfPrivacy/selfprivacy-rest-api/pulls/196 merge
 
 
 def set_kanidm_minimum_credential_type(minimum_credential_type: KanidmCredentialType):
-    with WriteUserData() as data:
-        if "kanidm" not in data:
-            logger.warning("Missing 'kanidm' key in /etc/nixos/userdata.json.")
+    result = subprocess.check_output(
+        [
+            "kanidm",
+            "group",
+            "account-policy",
+            "credential-type-minimum",
+            "idm_all_persons",
+            minimum_credential_type,
+        ],
+        stderr=subprocess.STDOUT,
+    ).decode("utf-8", "replace")
 
-            data["kanidm"] = {"minimum_credential_type": minimum_credential_type.value}
+    if "Updated credential type minimum" in result:
+        logger.warning(
+            f"Missing 'Updated credential type minimum' in 'kanidm group account-policy credential-type-minimum idm_all_persons {minimum_credential_type}' output"
+        )
 
-        elif "minimum_credential_type" not in data["kanidm"]:
-            logger.warning(
-                "Missing 'minimum_credential_type' key in /etc/nixos/userdata.json."
-            )
-
-        data["kanidm"]["minimum_credential_type"] = minimum_credential_type.value
+    if get_kanidm_minimum_credential_type() != minimum_credential_type:
+        raise RuntimeError()  # TODO: replace this error after https://git.selfprivacy.org/SelfPrivacy/selfprivacy-rest-api/pulls/196 merge
