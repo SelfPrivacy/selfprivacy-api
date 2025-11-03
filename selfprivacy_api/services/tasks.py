1a99c880243154632ad2fe0038d5bd7bc0b21385
nhnn <nhnn@nhnn.dev>
2025-11-03 12:08:42 +0300

    feat: run suggestedservices.sync on cron timer

--- selfprivacy_api/services/tasks.py
+++ selfprivacy_api/services/tasks.py
@@ -1,8 +1,16 @@
+from huey import crontab
+
 from selfprivacy_api.services import Service
+from selfprivacy_api.services.suggested import SuggestedServices
 from selfprivacy_api.utils.block_devices import BlockDevice
 from selfprivacy_api.utils.huey import huey, huey_async_helper
 from selfprivacy_api.jobs import Job, Jobs, JobStatus
 
+SUGGESTED_SERVICES_SYNC_EVERY_HOURS = 12
+
+@huey.periodic_task(crontab(hour="*/" + str(SUGGESTED_SERVICES_SYNC_EVERY_HOURS), minute="0"))
+def suggested_services_sync():
+    huey_async_helper.run_async(SuggestedServices.sync())
 
 @huey.task()
 def move_service(service: Service, new_volume: BlockDevice, job: Job) -> bool:
