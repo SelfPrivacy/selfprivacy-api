708e78122fc608e3153fb60a36a6279007f4f951
nhnn <nhnn@nhnn.dev>
2025-11-11 14:53:40 +0300

    fix: fix logging

--- selfprivacy_api/utils/memory_profiler.py
+++ selfprivacy_api/utils/memory_profiler.py
@@ -8,10 +8,9 @@ logger = logging.getLogger(__name__)
 async def memory_profiler_task():
     if tracemalloc.is_tracing():
         while True:
-            await asyncio.sleep(60 * 5)
             snapshot = tracemalloc.take_snapshot()
             top_lines = snapshot.statistics("lineno")
-            logger.info(
-                "API memory stats",
-                extra={"top_allocations": [str(line) for line in top_lines[:20]]},
-            )
+            logger.info("=== API memory allocations overview ===")
+            for line in top_lines[:20]:
+                logger.info(str(line))
+            await asyncio.sleep(60 * 5)
