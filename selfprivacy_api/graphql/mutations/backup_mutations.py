a56b3e60ad6904c549eeb5aa8004aa5c997fd2cb
nhnn <nhnn@nhnn.dev>
2025-11-25 16:51:49 +0300

    fix: add await in few places in graphql/mutations/backup_mutations.py

--- selfprivacy_api/graphql/mutations/backup_mutations.py
+++ selfprivacy_api/graphql/mutations/backup_mutations.py
@@ -103,7 +103,7 @@ class BackupMutations:
             )
 
     @strawberry.mutation(permission_classes=[IsAuthenticated])
-    def remove_repository(self) -> GenericBackupConfigReturn:
+    async def remove_repository(self) -> GenericBackupConfigReturn:
         """Remove repository"""
         with tracer.start_as_current_span("remove_repository"):
             Backups.reset()
@@ -111,7 +111,7 @@ class BackupMutations:
                 success=True,
                 message="",
                 code=200,
-                configuration=Backup().configuration(),
+                configuration=await Backup().configuration(),
             )
 
     @strawberry.mutation(permission_classes=[IsAuthenticated])
@@ -179,7 +179,7 @@ class BackupMutations:
                     success=False,
                     message=type(e).__name__ + ":" + str(e),
                     code=400,
-                    configuration=Backup().configuration(),
+                    configuration=await Backup().configuration(),
                 )
 
     @strawberry.mutation(permission_classes=[IsAuthenticated])
