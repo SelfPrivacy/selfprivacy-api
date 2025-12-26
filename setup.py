b5db576de283646e4b37474896566c13fe59ae4d
Inex Code <inex.code@selfprivacy.org>
2025-12-26 23:43:39 +0300

    chore: Bump version to 3.7.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selfprivacy_api",
-    version="3.7.1",
+    version="3.7.2",
     packages=find_packages(),
     scripts=[
         "selfprivacy_api/app.py",
